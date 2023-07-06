034f2e99e49680021d506132fd8aad2cfb10e2ee
codenvibes <terrymk1999@gmail.com>
2023-07-06 08:51:28 +0300

    Create a chat <li> element with passed mesage and className

--- Chatbot_HTML_CSS_JS/script.js
+++ Chatbot_HTML_CSS_JS/script.js
@@ -3,9 +3,20 @@ const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage;
 
+const createChatLi = (message, className) => {
+    // 
+    const chatLi = document.createElement("li");
+    chatLi.classList.add("chat", className);
+    let chatContent = className === "outgoing" ? `<span class="material-symbols-outlined">smart_toy</span><p>${message}</p>` : `<p>${message}</p>`;
+    chatLi.innerHTML = chatcontent;
+    return chatLi;
+}
+
 const handleChat = () => {
     userMessage = chatInput.value.trim();
-    console.log(userMessage);
+    if(!userMessage) return;
+
+    createChatLi(userMessage, "outgoing");
 }
 
 sendChatBtn.addEventListener("click", handleChat);
