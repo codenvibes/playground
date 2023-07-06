6b9ae980a4d3771aff7429f3be44e8f3aa7727b8
codenvibes <terrymk1999@gmail.com>
2023-07-06 14:19:32 +0300

    Use OpenAI API to get the response to the message
    Encountered an error while trying to link the OpenAI API 'You exceeded your current quota, please check your plan and billing details'

--- Chatbot_HTML_CSS_JS/script.js
+++ Chatbot_HTML_CSS_JS/script.js
@@ -3,6 +3,7 @@ const sendChatBtn = document.querySelector(".chat-input span");
 const chatbox = document.querySelector(".chatbox");
 
 let userMessage;
+const API_KEY = "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";
 
 const createChatLi = (message, className) => {
     // 
@@ -11,6 +12,30 @@ const createChatLi = (message, className) => {
     let chatContent = className === "outgoing" ? `<p>${message}</p>` : `<span class="material-symbols-outlined">smart_toy</span><p>${message}</p>`;
     chatLi.innerHTML = chatContent;
     return chatLi;
+}
+
+const generateResponse = () => {
+    const API_URL = "https://api.openai.com/v1/chat/completions";
+
+    // Define the properties and message for the API request
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${API_KEY}`
+        },
+        body: JSON.stringify({
+            "model": "gpt-3.5-turbo",
+            "messages": [{"role": "user", "content": userMessage}]
+        })
+    }
+
+    // Send POST request to API, get response
+    fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
+        console.log(data);
+    }).catch((error) => {
+        console.log(error);
+    })
 }
 
 const handleChat = () => {
@@ -23,6 +48,7 @@ const handleChat = () => {
     setTimeout(() => {
         // Display "Thinking..." message while waiting for the response
         chatbox.appendChild(createChatLi("Thinking...", "incoming"));
+        generateResponse();
     }, 600);
 }
 
